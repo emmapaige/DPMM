3f285ccec3b3f653c6efcc5048740031f1b366ce
emmapaige <112811131+emmapaige@users.noreply.github.com>
2024-03-24 18:34:32 -0400

    Update submit_test_influenza_1.sh to add log directories

--- submit_test_influenza_1.sh
+++ submit_test_influenza_1.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-#SBATCH -t 3- 
+#SBATCH -t 11- 
 #SBATCH -N 1 
 #SBATCH -n 1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=YOUR_EMAIL_HERE
 #SBATCH --output=./slurmlogs/slurm-%j.out
+
+# Create Rlogs and slurmlogs directories if they do not exist
+mkdir -p ./Rlogs
+mkdir -p ./slurmlogs
 
 module add r/4.1.3
 
-R CMD BATCH --vanilla --args --shortname=Test --core=1 --copy=1 --user=YOUR_USERNAME_HERE Influenza_Test_1.R ./Rlogs/Influenza_Test_1.out
+# Replace YOUR_USERNAME_HERE with your actual username
+R CMD BATCH --vanilla --args --shortname=Test --core=2 --copy=1 --user=YOUR_USERNAME_HERE Influenza_Test_1.R ./Rlogs/Influenza_Test_1.out
